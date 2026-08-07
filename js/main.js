b6b7fa829a2f0e5c6fc057d4dfb696728fa56c47
tomo-ui <tokonwicki@gmail.com>
2026-08-07 16:15:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -51,7 +51,7 @@
   */
   
   (function heroSlideshow() {
-    const heroModelki = GALLERY.filter((item) => item.category === "modelki").map((item) => item.full);
+    const heroModelki = GALLERY.filter((item) => item.category === "").map((item) => item.slideshow);
     if (heroModelki.length < 2) return;
 
     const startSrc = heroLayers[0].getAttribute("src");
